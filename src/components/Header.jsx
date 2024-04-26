dec7998531ce67836e0758c76bc3b45d3f2052b8
Harrison Chow <harrychow@Harrisons-MacBook-Pro-2.local>
2024-04-26 18:12:02 +1000

    responsive changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,11 +9,15 @@ import {
     ModalBody,
     ModalCloseButton,
 } from '@chakra-ui/react'
+import { useBreakpointValue } from '@chakra-ui/react';
 
 const Header = () => {
     const [instructionsOpen, setInstructionsOpen] = React.useState(true);
     const [aboutOpen, setAboutOpen] = React.useState(false);
     const [currentDate, setCurrentDate] = React.useState('');
+    const size = useBreakpointValue({ base: 'xs', md: 'md' });
+    const spacing = useBreakpointValue({ base: '5px', md: '20px' });
+    const heights = useBreakpointValue({ base: '40px', md: '80px' });
 
     React.useEffect(() => {
         // Get the current date in AEST
@@ -24,16 +28,16 @@ const Header = () => {
     return (
         <React.Fragment>
             {/* Grid containing title and buttons */}
-            <SimpleGrid columns={1} spacing="20px">
+            <SimpleGrid columns={1} spacing={spacing}>
                 <SimpleGrid columns={3} autoColumns>
-                    <Box height='80px' className='subheading-text'>
-                        <Button variant='ghost' className='align-with-title' onClick={() => setInstructionsOpen(true)}>
+                    <Box height={heights} className='subheading-text'>
+                        <Button variant='ghost' className='align-with-title' onClick={() => setInstructionsOpen(true)} size={size}>
                             How to play
                         </Button>
                     </Box>
-                    <Box height='80px' className='header-title'>Lingo</Box>
-                    <Box height='80px' className='subheading-text'>
-                        <Button variant='ghost' className='align-with-title' onClick={() => setAboutOpen(true)}>
+                    <Box height={heights} className='header-title'>Lingo</Box>
+                    <Box height={heights} className='subheading-text'>
+                        <Button variant='ghost' className='align-with-title' onClick={() => setAboutOpen(true)} size={size}>
                             About
                         </Button>
                     </Box>
@@ -54,7 +58,7 @@ const Header = () => {
                         <p>You get hints after each mistake, but 3 strikes and you're out.</p>
                     </ModalBody>
                     <ModalFooter>
-                        <Button colorScheme='green' onClick={() => setInstructionsOpen(false)}>Comprehendere</Button>
+                        <Button colorScheme='green' onClick={() => setInstructionsOpen(false)} size={size}>Comprehendere</Button>
                     </ModalFooter>
                 </ModalContent>
             </Modal>
@@ -69,11 +73,11 @@ const Header = () => {
                         <br />
                         <p>Inspired by continuous learning and trending NY Times games, we decided to provide a daily challenge that passionate linguists can complete in their day to day life.</p>
                         <br />
-                        <p style={{ fontSize: "12pt" }}>Developed by Harry and Caleb</p>
-                        <p style={{ fontSize: "12pt" }}>Send your feedback to harrisonmchow@gmail.com</p>
+                        <p>Developed by Harry and Caleb</p>
+                        <p>Send your feedback to harrisonmchow@gmail.com</p>
                     </ModalBody>
                     <ModalFooter>
-                        <Button onClick={() => setAboutOpen(false)}>Au revoir</Button>
+                        <Button onClick={() => setAboutOpen(false)} size={size}>Au revoir</Button>
                     </ModalFooter>
                 </ModalContent>
             </Modal>
